eaa7b3605285621c3c596940d8b257e5f409257d
mortinelli <seer_os@mail.ru>
2018-04-01 03:46:00 +0300

    date delemiter _

--- task4_3.sh
+++ task4_3.sh
@@ -16,13 +16,13 @@ elif ! [[ $2 =~ $re ]] || [ $2 -le 0 ]; then
 	fi
 
 	archname=$(echo $1 | sed -r 's/\//-/g' | cut -c 2-)
-	datetime=`date +%Y-%m-%d--%H%M%S`
+	datetime=`date +%Y_%m_%d_%H%M%S`
 	tar -zcf /tmp/backups/"$archname""$datetime".tar.gz "$1" > /dev/null
-	archcount=$(ls -1 /tmp/backups/ | grep -c "$archname[0-9]*-[0-9]*-[0-9]*--[0-9]*" )
+	archcount=$(ls -1 /tmp/backups/ | grep -c "$archname[0-9]*_[0-9]*_[0-9]*_[0-9]*" )
 
 	if [ $archcount -gt $2 ] ; then
 	let head="$archcount-$2"
-	ls -1 -d /tmp/backups/* | grep "$archname[0-9]*-[0-9]*-[0-9]*--[0-9]*" | head -$head | xargs -d '\n' rm
+	ls -1 -d /tmp/backups/* | grep "$archname[0-9]*_[0-9]*_[0-9]*_[0-9]*" | head -$head | xargs -d '\n' rm
 		fi
 
   fi
